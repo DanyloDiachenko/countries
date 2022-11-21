b70b26d7f3ab88a05aea6c01be5772b8f858e994
Danylo Diachenko <danildiachenko23@gmail.com>
2022-11-21 21:47:54 +0200

    added header, theme changes

--- countries/src/components/Header.jsx
+++ countries/src/components/Header.jsx
@@ -1,30 +1,61 @@
+import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { IoMoonOutline, IoMoon } from 'react-icons/io5';
 import { Container } from './Container';
 
-const HeaderEl = styled.header``;
+const HeaderEl = styled.header`
+    box-shadow: var(--shadow);
+    background-color: (--colors-ui-base);
+`;
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 2rem 0;
+`;
 
 const Title = styled.a.attrs({
     href: '/',
-})``;
+})`
+    color: var(--colors-text);
+    font-size: var(--fs-sm);
+    text-decoration: none;
+    font-weight: var(--fw-bold);
+`;
 
-const ModeSwitcher = styled.div``;
+const ModeSwitcher = styled.div`
+    color: var(--colors-text);
+    font-size: var(--fs-sm);
+    cursor: pointer;
+    text-transform: capitalize;
+    display: flex;
+    align-items: center;
+`;
 
-const Header = () => {
+export const Header = () => {
+
+    const [theme, setTheme] = useState('light');
+
+    useEffect(() => {
+        document.body.setAttribute('data-theme', theme);
+    }, [theme]);
+
     return (
         <HeaderEl>
             <Container>
                 <Wrapper>
-                    <Title>Where is the world?</Title>
-                    <ModeSwitcher>
-                        <IoMoon /> Light Theme
+                    <Title>Where is the world?</Title>            
+                    <ModeSwitcher onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>
+                        {theme === 'light' ? (
+                            <IoMoonOutline size='14px' />
+                        ) : (
+                            <IoMoon size='14px' />
+                        )}               
+                    <span style={{marginLeft: '0.75rem'}}>{theme} Theme</span>
                     </ModeSwitcher>
                 </Wrapper>
             </Container>
         </HeaderEl>
     );
 };
-
-export default Header;
