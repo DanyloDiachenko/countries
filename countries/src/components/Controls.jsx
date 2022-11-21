73dd45eeebdaf0c27ae803a492c321fc6c507d5d
Danylo Diachenko <danildiachenko23@gmail.com>
2022-11-21 22:36:54 +0200

    style fixes

--- countries/src/components/Controls.jsx
+++ countries/src/components/Controls.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import styled from 'styled-components';
 import { Search } from './Search';
 import { CustomSelect } from './CustomSelect';
 
@@ -10,15 +11,27 @@ const options = [
     { value: 'Oceania', label: 'Oceania' },
 ];
 
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: text-start;
+
+    @media(min-width: 767px) {
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+    };
+`;
+
 export const Controls = () => {
 
     const [search, setSearch] = useState('');
     const [region, setRegion] = useState('');
 
     return (
-        <div>
+        <Wrapper>
             <Search search={search} setSearch={setSearch} />
             <CustomSelect value={region} onChange={setRegion} options={options} placeholder="Filter by region" isClearable isSearchable={false} />
-        </div>
+        </Wrapper>
     );
 };
--- countries/src/components/CustomSelect.jsx
+++ countries/src/components/CustomSelect.jsx
@@ -1,5 +1,5 @@
-import styled from 'styled-components';
 import Select from 'react-select';
+import styled from 'styled-components';
 
 export const CustomSelect = styled(Select).attrs({
     styles: {
@@ -11,15 +11,36 @@ export const CustomSelect = styled(Select).attrs({
             padding: '0.25rem',
             border: 'none',
             boxShadow: 'var(--shadow)',
-            height: '50px'
+            height: '50px',
         }),
         option: (provided, state) => ({
             ...provided,
             cursor: 'pointer',
             color: 'var(--colors-text)',
-            backgroundColor: state.isSelected ? 'var(--colors-bg)' : 'var(--colors--ui-base)'
+            backgroundColor: state.isSelected
+                ? 'var(--colors-bg)'
+                : 'var(--colors-ui-base)',
         }),
-    }
+    },
 })`
+    width: 200px;
+    border-radius: var(--radii);
+    font-family: var(--family);
+    border: none;
 
+    & > * {
+        box-shadow: var(--shadow);
+    };
+
+    & input {
+        padding-left: 0.25rem;
+    };
+
+    & * {
+        color: var(--colors-text) !important;
+    };
+
+    & > div[id] {
+        background-color: var(--colors-ui-base);
+    };
 `;
