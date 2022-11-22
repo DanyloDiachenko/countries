447895e8efa995f3e761d1d302e910b9f6e401dd
Danylo Diachenko <danildiachenko23@gmail.com>
2022-11-22 16:57:56 +0200

    added countries

--- countries/src/App.jsx
+++ countries/src/App.jsx
@@ -2,12 +2,16 @@ import { useState, useEffect } from 'react';
 import { Controls } from './components/Controls';
 import { Header } from './components/Header';
 import { Main } from './components/Main';
+import { List } from './components/List';
+import { Card } from './components/Card';
 import { ALL_COUNTRIES } from './config';
 
 const App = () => {
 
   const [countries, setCountries] = useState([]);
-  
+
+
+  console.log(countries)
   useEffect(() => {
     fetch(ALL_COUNTRIES)
     .then((res) => res.json())
@@ -19,6 +23,32 @@ const App = () => {
       <Header />
       <Main>
         <Controls />
+        <List>
+          {countries.map((country) => {
+            const countryInfo = {
+              img: country.flags.png,
+              name: country.name,
+              info: [
+                {
+                  title: 'Population',
+                  description: country.population.toLocaleString(),
+                },
+                {
+                  title: 'Region',
+                  description: country.region,
+                },
+                {
+                  title: 'Capital',
+                  description: country.capital,
+                },
+              ],
+            };
+
+            return (
+              <Card key={country.name} {...countryInfo} />
+            )
+          })}
+        </List>
       </Main>
     </>
   );
