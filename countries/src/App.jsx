16db6d1160b8d086b81bef22d40d0a4c2a61f1ee
Danylo Diachenko <danildiachenko23@gmail.com>
2022-11-22 16:19:06 +0200

    added fetch, config fixes

--- countries/src/App.jsx
+++ countries/src/App.jsx
@@ -1,8 +1,19 @@
+import { useState, useEffect } from 'react';
 import { Controls } from './components/Controls';
 import { Header } from './components/Header';
 import { Main } from './components/Main';
+import { ALL_COUNTRIES } from './config';
 
 const App = () => {
+
+  const [countries, setCountries] = useState([]);
+  
+  useEffect(() => {
+    fetch(ALL_COUNTRIES)
+    .then((res) => res.json())
+    .then((data) => setCountries(data))
+  }, []);
+
   return (
     <>
       <Header />
--- countries/src/config.js
+++ countries/src/config.js
@@ -1,7 +1,7 @@
-const BASE_URL = 'https://restcountries.com/v2/all';
+const BASE_URL = 'https://restcountries.com/v2/';
 
-export const ALL_COUNTRIES = BASE_URL + 'all';
+export const ALL_COUNTRIES = BASE_URL + 'all?fields=name,capital,flags,population,region';
 
-export const searchByCountry = (name) => BASE_URL + '/name/' + name;
+export const searchByCountry = (name) => BASE_URL + 'name/' + name;
 
-export const filterByCode = (codes) => BASE_URL + 'alpha?codes=' + codes.join(', ');
+export const filterByCode = (codes) => BASE_URL + 'alpha?codes=' + codes.join(',');
