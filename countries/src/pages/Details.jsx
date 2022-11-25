17474483a4afc7e7f742d048a15ce486d1eb97db
Danylo Diachenko <danildiachenko23@gmail.com>
2022-11-25 16:35:25 +0200

    adding country description

--- countries/src/pages/Details.jsx
+++ countries/src/pages/Details.jsx
@@ -20,12 +20,90 @@ const Button = styled.button`
     cursor: pointer;
 `;
 
+const Wrapper = styled.section`
+    margin-top: 3rem;
+    width: 100%;
+    display: grid;
+    grid-template-columns: 100%;
+    gap: 2rem;
+
+    @media(min-width: 767px) {
+        grid-template-columns: minmax(100px, 400px) 1fr;
+        align-items: center;
+        gap: 5rem;
+    };
+
+    @media(min-width: 1024px) {
+        grid-template-columns: minmax(400px, 600px) 1fr;
+    };
+`;
+
+const CImage = styled.img`
+    display: block;
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+`;
+
+const CTitle = styled.h1`
+    margin: 0;
+    font-weight: var(--fw-normal);
+`;
+
+const ListGroup = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    gap: 2rem;
+
+    @media(min-width: 1024px) {
+        flex-direction: row;
+        gap: 4rem;
+    };
+`;
+
+const List = styled.ul`
+    list-style: none;
+    margin: 0;
+    padding: 0;
+
+    @media()
+`;
+
+const ListItem = styled.li`
+    line-height: 1.8;
+
+    & > b {
+        font-weight: var(--fw-bold)
+    };
+`;
+
+const Meta = styled.div`
+    margin-top: 3rem;
+    display: flex;
+    gap: 1.5rem;
+    flex-direction: column;
+    align-items: flex-start;
+
+    & > b {
+        font-weight: var(--fw-bold);
+    };
+`;
+
+const TagGroup = styled.div`
+
+`;
+
+const Tag = styled.span`
+
+`;
+
 export const Details = () => {
 
     const params = useParams();
     const navigate = useNavigate();
 
-    const [countryInfo, setCountryInfo] = useState(null);
+    const [c, setCountryInfo] = useState(null);
 
     useEffect(() => {
         fetch(searchByCountry(params.name))
@@ -33,16 +111,66 @@ export const Details = () => {
             .then((data) => setCountryInfo(data[0]))
     }, []);
 
-    const { name } = countryInfo;
-
     return (
         <>
-            {console.log(countryInfo)}
-            {!countryInfo ? <h1>Loading...</h1> : (
+            {console.log(c)}
+            {!c ? <h1>Loading...</h1> : (
                 <>
-                    <Button onClick={() => navigate(-1)}><BiArrowBack />Back</Button> {name}
+                    <Button onClick={() => navigate(-1)}><BiArrowBack />Back</Button>
 
+                    <Wrapper>
+                        <CImage src={c.flag} alt={c.name} />
 
+                        <div>
+                            <CTitle>{c.name}</CTitle>
+                            <ListGroup>
+                                <List>
+                                    <ListItem>
+                                        <b>Native Name: </b>{c.nativeName}
+                                    </ListItem>
+                                    <ListItem>
+                                        <b>Population: </b>{c.population}
+                                    </ListItem>
+                                    <ListItem>
+                                        <b>Region: </b>{c.region}
+                                    </ListItem>
+                                    <ListItem>
+                                        <b>Sub Region: </b>{c.subregion}
+                                    </ListItem>
+                                    <ListItem>
+                                        <b>Capital: </b>{c.capital}
+                                    </ListItem>
+                                </List>
+                                <List>
+                                    <ListItem>
+                                        <b>Top Level Domain: </b>{c.topLevelDomain}
+                                    </ListItem>
+                                    <ListItem>
+                                        <b>Currency: </b> {c.currencies.map((c) => (
+                                            <span key={c.code}>{c.name} </span>
+                                        ))}
+                                    </ListItem>
+                                    <ListItem>
+                                        <b>Languages: </b>{c.languages.map((l) => (
+                                            <span key={l.name}>{l.name} </span>
+                                        ))}
+                                    </ListItem>
+                                </List>
+                            </ListGroup>
+                            <Meta>
+                                <b>Border Countries:</b>
+                                {!c.borders.length ? (
+                                    <span>There is no border countries</span>
+                                ) : (
+                                    <TagGroup>
+                                        {c.borders.map((b) => (
+                                            <Tag key={b}>{b}</Tag>
+                                        ))}
+                                    </TagGroup>
+                                )}
+                            </Meta>
+                        </div>
+                    </Wrapper>
                 </>
             )}
         </>
