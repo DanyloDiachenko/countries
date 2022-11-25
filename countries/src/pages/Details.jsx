884c5d7dfe7e7d31a516e7c8282956eb3cdae0c6
Danylo Diachenko <danildiachenko23@gmail.com>
2022-11-25 15:36:13 +0200

    Update Details.jsx

--- countries/src/pages/Details.jsx
+++ countries/src/pages/Details.jsx
@@ -1,12 +1,31 @@
-import { useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import { searchByCountry } from '../config';
 
 export const Details = () => {
 
-    const params = useParams()
+    const params = useParams();
+    const navigate = useNavigate();
+
+    const [countryInfo, setCountryInfo] = useState(null);
+
+    useEffect(() => {
+        fetch(searchByCountry(params.name))
+            .then((res) => res.json())
+            .then((data) => setCountryInfo(data[0]))
+    }, []);
 
     return (
         <>
-            <h1>Details: {params.name}</h1>
+            <h1>Details:</h1>
+            {console.log(countryInfo)}
+            {!countryInfo ? <h1>Loading...</h1> : (
+                <>
+                    <h2>{countryInfo.name}</h2>
+                    <img src={countryInfo.flags.png} />
+                    <button onClick={() => navigate(-1)}>go back</button>
+                </>
+            )}
         </>
     );
 };
