e39f98c01b6003814ef10bfce14b5a3377f2c2f6
Danylo Diachenko <danildiachenko23@gmail.com>
2022-11-25 15:58:00 +0200

    Update Details.jsx

--- countries/src/pages/Details.jsx
+++ countries/src/pages/Details.jsx
@@ -1,6 +1,24 @@
 import { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
+import { BiArrowBack } from 'react-icons/bi';
+import styled from 'styled-components';
 import { searchByCountry } from '../config';
+
+const Button = styled.button`
+    width: 85px;
+    height: 35px;
+    padding: 0 15px;
+    border-radius: var(--radii);
+    border: none;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin: 30px 0 30px 0;
+    background: var(--colors-ui-base);
+    color: var(--colors-text);
+    box-shadow: var(--shadow);
+    cursor: pointer;
+`;
 
 export const Details = () => {
 
@@ -15,15 +33,16 @@ export const Details = () => {
             .then((data) => setCountryInfo(data[0]))
     }, []);
 
+    const { name } = countryInfo;
+
     return (
         <>
-            <h1>Details:</h1>
             {console.log(countryInfo)}
             {!countryInfo ? <h1>Loading...</h1> : (
                 <>
-                    <h2>{countryInfo.name}</h2>
-                    <img src={countryInfo.flags.png} />
-                    <button onClick={() => navigate(-1)}>go back</button>
+                    <Button onClick={() => navigate(-1)}><BiArrowBack />Back</Button> {name}
+
+
                 </>
             )}
         </>
