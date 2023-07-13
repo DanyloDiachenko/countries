2f7f79b4f119ff1fb4b3198c8b157e3ff6f10499
Danylo Diachenko <danildiachenko23@gmail.com>
2023-07-13 13:42:59 +0300

    fixes

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
-import { ICountry } from './interfaces/country.interface';
+import { ICountry } from "./interfaces/country.interface";
 
-const BASE_URL: string = "https://restcountries.com/v2/";
+const BASE_URL: string = process.env.REACT_APP_API_URL || "";
 
 export const ALL_COUNTRIES: string =
     BASE_URL + "all?fields=name,capital,flags,population,region";
